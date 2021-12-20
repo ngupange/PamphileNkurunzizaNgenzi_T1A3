76060fbf4c502d27b6140725dfbbb17c1469b0a9
ngupange <gcab022120@coderacademy.edu.au>
2021-12-20 19:54:13 +1000

    Repo Initiarization

---
+++ src/init.rb
@@ -0,0 +1,11 @@
+#### Directory Maker ####
+#
+# Launch this Ruby file from the command line
+# to get started
+
+APP_ROOT = File.expand_path(File.dirname(__FILE__))
+
+require_relative 'lib/controller'
+
+controller = ListMaker::Controller.new
+controller.launch!
---
+++ src/lib/controller.rb
@@ -0,0 +1,108 @@
+require_relative 'list_dir'
+require_relative 'list_file'
+
+require "tty-prompt"
+$prompt = TTY::Prompt.new
+
+module ListMaker
+  class Controller
+
+    @@valid_actions = ['view', 'add', 'edit', 'load', 'quit']
+    
+    def initialize
+      @list_dir = ListMaker::ListDir.new
+    end
+
+    def select_options
+      answer = $prompt.select("What do you want to do?", ['View contents', 'Add New Record', 'Edit File Content ', 'Load a File to Manipulate', 'Quit'])
+    return answer
+    end
+
+    def launch!
+      introduction
+      
+      do_action('load', [])
+
+      # input/action loop
+      loop do
+        action, args = get_action
+        break if action == 'quit'
+        result = do_action(action,args)
+      end
+      conclusion
+    end
+    
+    private
+    
+      def introduction
+        puts "-" * 100
+        puts "Directories Maker".upcase.center(80)
+        puts "-" * 100
+        puts "This is an interactive program helps users to create and manage lists."
+      end
+
+      def conclusion
+        puts
+        puts "-" * 100
+        puts "Goodbye!".upcase.center(80)
+        puts "-" * 100
+        puts
+      end
+    
+      def get_action
+        action = nil
+        @con = ListMaker::ListDir.new
+        response = select_options.chomp
+        args = response.downcase.strip.split(' ')
+        action = args.shift
+        if action == "edit"
+          args = nil
+          while args == nil         
+            system "clear"
+            lim = @con.fnbr.length
+            puts "choose number from list"
+            print "> "
+            results = gets.chomp.to_i
+            if (results <= lim) && (results > 0)
+              args = results.to_s
+              args = args.split()
+            else
+              puts "Wrong answer, please pick right list number"
+            end
+          end
+        end
+        [action, args]
+      end
+        
+      def do_action(action, args)
+        case action
+        when 'view'
+          system "clear"
+          print "#{@list_file} : "
+          @list_file.view
+          puts "\n \n"
+        when 'add'
+          system "clear"
+          puts "\n"
+          @list_file.view
+          @list_file.add
+        when 'edit'
+          system "clear"
+          @list_file.view
+          @list_file.edit(args)
+          puts "\n \n"
+        when 'load'
+          system "clear"
+          # Use a different list file
+          new_file = @list_dir.choose_list
+          @list_file = ListMaker::ListFile.new(new_file)
+          @list_file.view
+          puts "\n \n"
+        else
+          puts "\nI don't understand that command.\n\n"
+        end
+      end
+      
+  end
+  
+end
